7641dac53b06242040193c01443ae9527a253dfd
Raúl Cumplido <raulcumplido@gmail.com>
2025-01-13 16:57:25 +0100

    Initial test for C++ loader call and use

---
+++ test_load.py
@@ -0,0 +1,12 @@
+import ctypes
+import pyarrow as pa
+import pyarrow.compute as pc
+
+# Load the shared library
+lib = ctypes.CDLL('/tmp/kernel-loader/libarrow_kernel_loader.so')
+res = lib.LoadKernels()
+assert res == 0
+array1 = pa.array([1,2])
+result = pc.call_function("custom_add", [array1, array1])
+
+assert result == pa.array([2, 4])
