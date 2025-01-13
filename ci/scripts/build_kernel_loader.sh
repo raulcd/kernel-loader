370a94b8325a88a0e78f4bedd3e9ce2b1e4d16b4
Raúl Cumplido <raulcumplido@gmail.com>
2025-01-13 18:08:39 +0100

    Define CMAKE_PREFIX_PATH from micromamba

--- ci/scripts/build_kernel_loader.sh
+++ ci/scripts/build_kernel_loader.sh
@@ -3,12 +3,15 @@ set -eux
 source_dir=${1}
 build_dir=${1}/build
 
+LOADER_CMAKE_PREFIX_PATH=${2:-}
+
 mkdir ${build_dir}
 pushd ${build_dir}
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=/tmp/kernel-loader \
     -DARROW_LINK_SHARED=ON \
+    -DCMAKE_PREFIX_PATH=${LOADER_CMAKE_PREFIX_PATH:-}
     ${source_dir}
 cmake --build . --target install
 
