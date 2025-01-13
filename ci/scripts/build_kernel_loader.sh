219f62b38a21963c8f791ce5443c2f4d99e01370
Raúl Cumplido <raulcumplido@gmail.com>
2025-01-13 18:44:43 +0100

    Remove unnecessary CMAKE_PREFIX_PATH

--- ci/scripts/build_kernel_loader.sh
+++ ci/scripts/build_kernel_loader.sh
@@ -3,15 +3,12 @@ set -eux
 source_dir=${1}
 build_dir=${1}/build
 
-LOADER_CMAKE_PREFIX_PATH=${2:-}
-
 mkdir -p ${build_dir}
 pushd ${build_dir}
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=/tmp/kernel-loader \
     -DARROW_LINK_SHARED=ON \
-    -DCMAKE_PREFIX_PATH=${LOADER_CMAKE_PREFIX_PATH:-} \
     ${source_dir}
 cmake --build .
 
