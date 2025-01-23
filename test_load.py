13a020eae38d4b05944993cbd9d0a6bd075a18a5
Raúl Cumplido <raulcumplido@gmail.com>
2025-01-23 10:17:38 +0100

    Add test for chunked array

--- test_load.py
+++ test_load.py
@@ -4,13 +4,15 @@ import pyarrow.compute as pc
 
 # Load the shared library
 lib = ctypes.CDLL('/home/runner/work/kernel-loader/kernel-loader/build/libarrow_kernel_loader.so')
+
 res = lib.LoadKernels()
 assert res == 0
 array1 = pa.array([2, 1 ,2])
-result = pc.call_function("custom_add", [array1, array1])
-
-assert result == pa.array([4, 2, 4])
-print(result)
 
 result = pc.call_function("vendored_rank_quantile", [array1])
 print(f"Rank Quantile: {result}")
+
+chunked_array1 = pa.chunked_array([[2, 1 ,2], [1,3]])
+
+result = pc.call_function("vendored_rank_quantile", [chunked_array1])
+print(f"Rank Quantile for chunked array: {result}")
