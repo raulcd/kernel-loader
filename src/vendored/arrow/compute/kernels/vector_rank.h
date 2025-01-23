f308131959f09b51a9b5ab91cd3b0957f4aa02f3
Raúl Cumplido <raulcumplido@gmail.com>
2025-01-23 11:36:20 +0100

    Remove unnecessary includes

--- src/vendored/arrow/compute/kernels/vector_rank.h
+++ src/vendored/arrow/compute/kernels/vector_rank.h
@@ -1,8 +1,7 @@
 #include <arrow/compute/registry.h>
-#include <arrow/compute/ordering.h>
-#include <arrow/compute/function_options.h>
 
 namespace arrow::compute::internal {
 
 void RegisterVectorRank(FunctionRegistry* registry);
+
 }  // namespace arrow::compute::internal
