0339ef5e9e46deb3bbf0a411e0189918bc4cb9d4
Raúl Cumplido <raulcumplido@gmail.com>
2025-02-12 13:13:34 +0100

    Minor nits and fix typo

--- src/load_kernels.h
+++ src/load_kernels.h
@@ -8,8 +8,6 @@ namespace arrow::compute::internal::vendored {
 
 arrow::Status LoadKernels(arrow::compute::FunctionRegistry* registry);
 
-
-
 }  // namespace arrow::compute::internal::vendored
 
 extern "C" int LoadKernels();
