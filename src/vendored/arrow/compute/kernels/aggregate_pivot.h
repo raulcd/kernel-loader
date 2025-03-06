0882c54808159c1560c4aa65d2559a490d2fbc9f
Raúl Cumplido <raulcumplido@gmail.com>
2025-03-06 14:25:27 +0100

    Add missing file

---
+++ src/vendored/arrow/compute/kernels/aggregate_pivot.h
@@ -0,0 +1,7 @@
+#include <arrow/compute/registry.h>
+
+namespace arrow::compute::internal {
+
+void RegisterScalarAggregatePivot(arrow::compute::FunctionRegistry* registry);
+
+}  // namespace arrow::compute::internal
