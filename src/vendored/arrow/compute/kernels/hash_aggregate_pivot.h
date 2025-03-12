52a42035ab3376913e51f5379d13eceee8931b6d
Raúl Cumplido <raulcumplido@gmail.com>
2025-03-12 10:51:20 +0100

    Apply patch to latest upstream main refactoring hash_pivot

--- src/vendored/arrow/compute/kernels/hash_aggregate_pivot.h
+++ src/vendored/arrow/compute/kernels/hash_aggregate_pivot.h
@@ -2,6 +2,6 @@
 
 namespace arrow::compute::internal::vendored {
 
-void RegisterHashAggregateBasic(arrow::compute::FunctionRegistry* registry);
+void RegisterHashAggregatePivot(arrow::compute::FunctionRegistry* registry);
 
 }  // namespace arrow::compute::internal::vendored
